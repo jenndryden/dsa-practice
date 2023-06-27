d0577eb8f7d928a5322ee7100b05e2d9b3066164
Jenn Dryden <jenndryden8@gmail.com>
2023-06-27 11:37:51 -0400

    Time: 49 ms (34.57%), Space: 16.2 MB (66.20%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,9 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         one, two = 1, 1 
-        nextvar = 1 
+        nextvar = 1
 
-        for i in range(n -1):
+        for i in range(n - 1):
             nextvar = one + two
             one = two
             two = nextvar
